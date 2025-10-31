d3338d0f0ead03fdc6bceee606341e7f74a8c02d
玩恐鬼的砂狼 <CNDormer@gmail.com>
2025-10-31 12:04:15 -0400

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,11 +1,11 @@
 const display=document.getElementById("display")
 
 function appendToDisplay(input) {
-
+  display.valuen += input;
 }
 
 function clearDisplay() {
-
+  display.value="0";
 }
 
 function calculate() {
