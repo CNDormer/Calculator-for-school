4c5f20dcd9a55b654fdaeca56b1171fae2661066
玩恐鬼的砂狼 <CNDormer@gmail.com>
2025-11-05 12:42:32 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,19 +1,59 @@
 const display = document.getElementById("display");
+let clearvalue = false;
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+    const lastChar = display.value.slice(-1);
+
+    if (display.value === "Error") {
+        display.value = "";
+        clearvalue = false;
+    }
+
+    if (display.value === "" && /[+\-*/^]/.test(input)) {
+        display.value = "0" + input;
+        clearvalue = false;
+        return;
+    }
+
+    if (clearvalue) {
+        if (/[0-9.]/.test(input)) {
+            display.value = input;
+            clearvalue = false;
+            return;
+        }
+        if (/[+\-*/^]/.test(input)) {
+            display.value = "0" + input;
+            clearvalue = false;
+            return;
+        }
+    }
+
+    if (/[+\-*/^]/.test(input)) {
+        if (!/[+\-*/^]/.test(lastChar)) {
+            display.value += input;
+        }
+    } 
+    else {
+        display.value += input;
+    }
 }
+
+
+
 
 function clearDisplay(){
     display.value = "";
+    clearvalue=false;
 }
 
 function calculate(){
     try{
         display.value = eval(display.value);
+        clearvalue=true;
+    }
+    catch(error){
+        display.value = "Error";
+        clearvalue=true
     }
 }
-
-function squareRoot(input) {
-  
-}
+/* Add /[0-9.]+/g with replace element for multiplycation and somthing else*/
