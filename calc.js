42e414f581d5dc420865c3b4324647e9deffd7ce
玩恐鬼的砂狼 <CNDormer@gmail.com>
2025-11-13 10:17:19 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -42,110 +42,116 @@ function appendToDisplay(input) {
     }
 }
 
-
-
-
-
-function clearDisplay(){
+function clearDisplay() {
     display.value = "";
-    clearvalue=false;
+    clearvalue = false;
 }
 
 function calculate() {
-    const maxDigit=12;
     try {
         let expression = display.value;
+        const sciMatches = [];
+        expression = expression.replace(/[0-9]+(?:\.[0-9]+)?E[+-]?\d+/gi, (m) => {
+            const key = `__SCI${sciMatches.length}__`;
+            sciMatches.push(m);
+            return key;
+        });
 
-        if (expression.includes("√")) {
-            expression = expression.replace(/√\(/g, "Math.sqrt(");
-            expression = expression.replace(/(\d)(Math\.sqrt)/g, "$1*$2");
-            expression = expression.replace(/\)(Math\.sqrt)/g, ")*$1");
-        }
-        expression = expression.replace(/(\d)\(/g, "$1*(");
-        expression = expression.replace(/\)(\d)/g, ")*$1");
-        expression = expression.replace(/([0-9.]+)\(/g, "$1*(");
+        expression = expression.replace(/√\(/g, "Math.sqrt(");
         expression = expression.replace(/sin\(/g, "Math.sin(Math.PI/180*");
         expression = expression.replace(/cos\(/g, "Math.cos(Math.PI/180*");
         expression = expression.replace(/tan\(/g, "Math.tan(Math.PI/180*");
-        let result = eval(expression);
-        if (typeof result === "number") {
-            const formatted = result.toPrecision(10);
-            display.value = parseFloat(formatted).toString();
-        } else {
-            display.value = result;
-        }
-        clearvalue = true;
 
-    } catch (error) {
+        expression = expression.replace(/([0-9πΠ𝑒])log\(/g, "$1*Math.log10(");
+        expression = expression.replace(/([0-9πΠ𝑒])ln\(/g, "$1*Math.log(");
+        expression = expression.replace(/(^|[^0-9πΠ𝑒])log\(/g, "$1Math.log10(");
+        expression = expression.replace(/(^|[^0-9πΠ𝑒])ln\(/g, "$1Math.log(");
+
+        expression = expression.replace(/π/g, "Math.PI");
+        expression = expression.replace(/Π/g, "(3.14)");
+        expression = expression.replace(/𝑒/g, "Math.E");
+        for (let i = 0; i < sciMatches.length; i++) {
+            expression = expression.replace(`__SCI${i}__`, sciMatches[i]);
+        }
+        expression = expression.replace(/(\d|\)|Math\.PI|Math\.E|3\.14)(\()/g, "$1*$2");
+        expression = expression.replace(/(\d)(Math\.)/g, "$1*$2");
+        expression = expression.replace(/\)(\d)/g, ")*$1");
+        expression = expression.replace(/\)(Math\.)/g, ")*$1");
+        expression = expression.replace(/(Math\.(?:PI|E|sqrt|log|log10))(\d)/g, "$1*$2");
+        expression = expression.replace(/(Math\.(?:PI|E))\(/g, "$1*(");
+        expression = expression.replace(/\^/g, "**");
+        let result = eval(expression);
+
+        if (typeof result === "number" && isFinite(result)) {
+            display.value = parseFloat(result.toPrecision(10)).toString();
+        } else {
+            display.value = "Error";
+        }
+
+        clearvalue = true;
+    } catch {
         display.value = "Error";
         clearvalue = true;
     }
 }
 
+
 function squareRoot() {
-    if (display.value === "Error") {
-        display.value = "";
-        clearvalue = false;
-    } else {
-        const lastChar = display.value.slice(-1);
-
-        if (/\d|\)/.test(lastChar)) {
-            display.value += "√(";
-        } else {
-            display.value += "√(";
-        }
-
-        clearvalue = false;
-    }
+    if (display.value === "Error") display.value = "";
+    display.value += "√(";
+    clearvalue = false;
 }
 
-function trigCalculateSin(){
-    if (display.value === "Error") {
-        display.value = "";
-        clearvalue = false;
-    } else {
-        const lastChar = display.value.slice(-1);
-
-        if (/\d|\)/.test(lastChar)) {
-            display.value +="sin(";
-        } else {
-            display.value +="sin(";
-        }
-
-        clearvalue = false;
-    }
+function trigCalculateSin() {
+    if (display.value === "Error") display.value = "";
+    display.value += "sin(";
+    clearvalue = false;
 }
 
-function trigCalculateCos(){
-    if (display.value === "Error") {
-        display.value = "";
-        clearvalue = false;
-    } else {
-        const lastChar = display.value.slice(-1);
-
-        if (/\d|\)/.test(lastChar)) {
-            display.value +="cos(";
-        } else {
-            display.value +="cos(";
-        }
-
-        clearvalue = false;
-    }
+function trigCalculateCos() {
+    if (display.value === "Error") display.value = "";
+    display.value += "cos(";
+    clearvalue = false;
 }
 
-function trigCalculateTan(){
-    if (display.value === "Error") {
-        display.value = "";
-        clearvalue = false;
-    } else {
-        const lastChar = display.value.slice(-1);
+function trigCalculateTan() {
+    if (display.value === "Error") display.value = "";
+    display.value += "tan(";
+    clearvalue = false;
+}
 
-        if (/\d|\)/.test(lastChar)) {
-            display.value +="tan(";
-        } else {
-            display.value +="tan(";
-        }
+function logFunction() {
+    if (display.value === "Error") display.value = "";
+    display.value += "log(";
+    clearvalue = false;
+}
 
-        clearvalue = false;
-    }
+function lnFunction() {
+    if (display.value === "Error") display.value = "";
+    display.value += "ln(";
+    clearvalue = false;
+}
+
+function piFunction() {
+    if (display.value === "Error") display.value = "";
+    display.value += "π";
+    clearvalue = false;
+}
+
+function eulerFunction() {
+    if (display.value === "Error") display.value = "";
+    display.value += "𝑒";
+    clearvalue = false;
+}
+
+function piButThreeDigits() {
+    if (display.value === "Error") display.value = "";
+    display.value += "Π";
+    clearvalue = false;
+}
+
+function sNotation() {
+    if (display.value === "Error") display.value = "";
+    display.value += "E";
+    clearvalue = false;
 }
